1cfc7c6019bcdf751262c46e601b87570cb40c78
Bedford761 <yasminebedford0@gmail.com>
2026-02-06 15:01:06 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#Request a user to input there name  
+#Print name 
+#Request a user to input there age
+#Print age 
+#Print hello world 
+
+name = input("Enter your name")
+print(name)
+age = input("Enter your age") 
+print(age) 
+print("Hello, World!")
